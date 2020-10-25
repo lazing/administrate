d23545cd8924a07624a8296cd26eb84eb1e8f28d
Ryan Wong <lazing@gmail.com>
2020-10-25 23:05:49 +0800

    remove ui dependencies

--- lib/administrate/engine.rb
+++ lib/administrate/engine.rb
@@ -1,10 +1,10 @@
-require "datetime_picker_rails"
-require "jquery-rails"
+# require "datetime_picker_rails"
+# require "jquery-rails"
 require "kaminari"
-require "momentjs-rails"
-require "sassc-rails"
-require "selectize-rails"
-require "sprockets/railtie"
+# require "momentjs-rails"
+# require "sassc-rails"
+# require "selectize-rails"
+# require "sprockets/railtie"
 
 require "administrate/page/form"
 require "administrate/page/show"
@@ -19,26 +19,26 @@ module Administrate
   class Engine < ::Rails::Engine
     isolate_namespace Administrate
 
-    @@javascripts = []
-    @@stylesheets = []
+    # @@javascripts = []
+    # @@stylesheets = []
 
-    def self.add_javascript(script)
-      @@javascripts << script
-    end
+    # def self.add_javascript(script)
+    #   @@javascripts << script
+    # end
 
-    def self.add_stylesheet(stylesheet)
-      @@stylesheets << stylesheet
-    end
+    # def self.add_stylesheet(stylesheet)
+    #   @@stylesheets << stylesheet
+    # end
 
-    def self.stylesheets
-      @@stylesheets
-    end
+    # def self.stylesheets
+    #   @@stylesheets
+    # end
 
-    def self.javascripts
-      @@javascripts
-    end
+    # def self.javascripts
+    #   @@javascripts
+    # end
 
-    add_javascript "administrate/application"
-    add_stylesheet "administrate/application"
+    # add_javascript "administrate/application"
+    # add_stylesheet "administrate/application"
   end
 end
